56c8255ef47f83e3e82ba467660e4d38dc92818e
Hardik <hardik.parmar15622@sakec.ac.in>
2023-03-18 11:32:10 +0530

    CSV changes

--- ui/app.py
+++ ui/app.py
@@ -106,12 +106,24 @@ def attendee(fsv):
 
     elif fsv.endswith('.csv'):
         # Read the CSV file using the csv module
-        with open(fsv, "r") as file:
-            csvReader = csv.reader(codecs.open(fsv, 'rU', 'utf-16'))
-        count = -1
-        for row in csvReader:
-            if row[attendee_column]:
-                count += 1
+        # with open(fsv, "r") as file:
+        #     csvReader = csv.reader(codecs.open(fsv, 'rUb', 'utf-16'))
+        # count = -1
+        # for row in csvReader:
+        #     if row[attendee_column]:
+        #         count += 1
+
+        unique_attendees = set()  # a set to store unique attendees
+
+        with open(fsv, 'r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+        # assuming the attendee name is in the first column of the CSV file
+                attendee = row[0].strip()  # remove leading/trailing whitespaces
+                if attendee:  # check if the attendee name is not an empty string
+                    unique_attendees.add(attendee)  # add the attendee name to the set of unique attendees
+
+        count = len(unique_attendees)
 
     else:
         raise ValueError("Unsupported file format")
